9f6cee16da85e03dfd665d8227af529a7230320d
zbygher <zbygher@gmail.com>
2018-11-16 13:28:36 +0200

    Added:
    	LEVEL 1:
    		Directional lights;
    		Floor;
    		Player;
              		Enemy objects;
    		Goal object;
    		Patrol points;
    		Materials;
    		PlayerMovement.cs and Patrol.cs SCRIPTS!
    
    	Alpha builds for Windows platform (in Builds folder):
    		A1;
    		A2.
    
    	Player (Blue Cube) now can move with keyboard arrows buttons. Player will respawn when touch Enemy (Red Cube).

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed;
+    private Vector3 input;
+    public Rigidbody rb;
+    private float maxSpeed = 5f;
+    private Vector3 spawn;
+
+    // Use this for initialization
+    void Start () {
+        spawn = transform.position;
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        if (rb.velocity.magnitude < maxSpeed)
+        {
+            rb.AddForce(input * moveSpeed);
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.tag == "Enemy")
+        {
+            transform.position = spawn;
+        }
+    }
+
+
+
+
+}
